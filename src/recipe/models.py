2054d252bd78997493957003666a62c517761b6f
unnamedKnight <backfrontdev47@gmail.com>
2022-11-29 18:49:17 +0600

    Add Ingredient model

--- src/recipe/models.py
+++ src/recipe/models.py
@@ -12,6 +12,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField("Tag")
+    ingredients = models.ManyToManyField("Ingredient")
 
     def __str__(self):
         return (f"{self.title} + {self.id}")
@@ -24,3 +25,13 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Ingredient(models.Model):
+    """Ingredient for Recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
